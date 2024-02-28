78ba0b6303cf4d4d971d5cc31ba58404f2cab662
jerrypm <jerrypurnamamaulid@gmail.com>
2024-02-28 22:07:12 +0700

    add template without folder

---
+++ JPMViperWTF.xctemplate/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ JRDEV. All rights reserved.
+
+import UIKit
+
+protocol I___VARIABLE_viperModuleName___ViewController: AnyObject {
+    // type here ...
+}
+
+class ___VARIABLE_viperModuleName___ViewController: UIViewController {
+    var presenter: I___VARIABLE_viperModuleName___ViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ___VARIABLE_viperModuleName___ViewController: I___VARIABLE_viperModuleName___ViewController {
+    // type here ...
+}
